4192546a262d60007040297a20737b330e2d0deb
D0000M <doom76upup@gmail.com>
2024-01-28 11:57:23 +0800

    实现编译中局部绑定

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -70,7 +70,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		c.emit(code.OpGetGlobal, symbol.Index) // 在字节码中将标识符替换
+		if symbol.Scope == GlobalScope { // 在字节码中将标识符替换
+			c.emit(code.OpGetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpGetLocal, symbol.Index)
+		}
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
@@ -211,7 +215,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 		symbol := c.symbolTable.Define(node.Name.Value) // 包含Index
-		c.emit(code.OpSetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpSetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpSetLocal, symbol.Index)
+		}
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
 		if err != nil {
@@ -399,11 +407,15 @@ func (c *Compiler) enterScope() {
 	}
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
 	instructions := c.currentInstructions()
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+
+	c.symbolTable = c.symbolTable.Outer
 	return instructions
 }
--- compiler/symboltable.go
+++ compiler/symboltable.go
@@ -4,6 +4,7 @@ type SymbolScope string
 
 const (
 	GlobalScope SymbolScope = "GLOBAL"
+	LocalScope  SymbolScope = "LOCAL"
 )
 
 type Symbol struct {
@@ -13,6 +14,8 @@ type Symbol struct {
 }
 
 type SymbolTable struct {
+	Outer *SymbolTable // 用于访问外层符号表
+
 	store          map[string]Symbol
 	numDefinitions int
 }
@@ -22,14 +25,40 @@ func NewSymbolTable() *SymbolTable {
 	return &SymbolTable{store: s}
 }
 
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	s := NewSymbolTable()
+	s.Outer = outer
+	return s
+}
+
 func (s *SymbolTable) Define(name string) Symbol {
-	symbol := Symbol{Name: name, Scope: GlobalScope, Index: s.numDefinitions}
+	symbol := Symbol{Name: name, Index: s.numDefinitions}
+
+	if s.Outer != nil {
+		symbol.Scope = LocalScope
+	} else {
+		symbol.Scope = GlobalScope
+	}
+
 	s.store[name] = symbol
 	s.numDefinitions++
 	return symbol
 }
 
 func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
+	// obj, ok := s.store[name]
+
+	// outer := s.Outer
+	// for !ok && outer != nil {
+	// 	obj, ok = outer.Resolve(name)
+	// 	outer = outer.Outer
+	// }
+
+	// 递归结构
 	obj, ok := s.store[name]
+	if !ok && s.Outer != nil { // 这一层找不到就向上找，然后立即返回
+		obj, ok = s.Outer.Resolve(name)
+		return obj, ok
+	}
 	return obj, ok
 }
